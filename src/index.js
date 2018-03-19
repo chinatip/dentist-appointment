8597327ccf259c3522ce2499ab87168ec0fb10fe
chinatip <chinatip.v@ku.th>
2018-03-20 02:33:17 +0700

    Add Login

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-import reducer from 'redux/reducers';
-import 'antd/dist/antd.css';
-import 'common/styles/global.css';
 import Clinic from 'clinic';
+import Login from './Login';
+import reducer from 'redux/reducers';
 import { Home, Appointment, Contact } from 'client'
 import registerServiceWorker from './registerServiceWorker';
+import 'antd/dist/antd.css';
+import 'common/styles/global.css';
 
 const middlewares = [thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -32,6 +33,7 @@ class App extends Component {
           <Router>
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
               <Route path="/appointment" component={Appointment} />
               <Route path="/contact" component={Contact} />
               <Route path="/login" component={Contact} />
