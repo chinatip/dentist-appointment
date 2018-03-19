85e73a186dad8e20f35cb77ac4108944b52d0d6b
chinatip <chinatip.v@ku.th>
2018-03-20 02:38:55 +0700

    Update routes

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import Clinic from 'clinic';
+import Login from './Login';
+import { Home, Appointment, Contact } from 'client'
+
+export default () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/appointment" component={Appointment} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/login" component={Contact} />
+        <Route exact path="/clinic" render={() => <Redirect to="/clinic/status" />} />
+        <Route path="/clinic/:type" component={Clinic} />
+      </Switch>
+    </Router>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import styled from 'styled-components';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-import Clinic from 'clinic';
-import Login from './Login';
+import Routes from './Routes'
 import reducer from 'redux/reducers';
-import { Home, Appointment, Contact } from 'client'
 import registerServiceWorker from './registerServiceWorker';
 import 'antd/dist/antd.css';
 import 'common/styles/global.css';
@@ -30,17 +27,7 @@ class App extends Component {
     return (
       <Provider store={store} key="provider">
         <Container>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/login" component={Login} />
-              <Route path="/appointment" component={Appointment} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/login" component={Contact} />
-              <Route exact path="/clinic" render={() => <Redirect to="/clinic/status" />} />
-              <Route path="/clinic/:type" component={Clinic} />
-            </Switch>
-          </Router>
+          <Routes />
         </Container>
       </Provider>
     );
