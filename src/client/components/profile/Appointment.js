4afc9a6d6c3e70f0768c57b331d55ac20c9bb903
chinatip <chinatip.v@ku.th>
2018-05-02 08:54:16 +0700

    Update Appointment

--- src/client/components/profile/Appointment.js
+++ src/client/components/profile/Appointment.js
@@ -5,8 +5,9 @@ import { compose } from 'recompose'
 import { connect } from 'react-redux'
 
 import { getUser } from 'redux/user'
+import { Table } from 'common'
 import { LOADER, FETCH, APPOINTMENT, FIND_BY_FB_ID } from 'services'
-
+import { stringToMoment } from 'common/utils'
 
 const enhance = compose(
   LOADER,
@@ -21,22 +22,53 @@ const AppItemContainer = styled.div`
 
 `
 
-const AppItem = ({ appointment }) => {
-  const { slot, status, report } = appointment
-  console.log(appointment)
-  return (
-    <AppItemContainer>
-      
-    </AppItemContainer>
-  )
+const formatAppointments = (appointments) => {
+  const columns = [
+    {
+      title: 'เวลา',
+      dataIndex: 'slot.startTime',
+      key: 'slot.startTime',
+      render: (t) => <p>{stringToMoment(t).format('DD MMM YYYY H:mm')}</p>
+    }, {
+      title: 'คลินิค',
+      dataIndex: 'slot.clinic.name',
+      key: 'slot.clinic.name'
+    }, {
+      title: 'การรักษา',
+      dataIndex: 'treatment.name',
+      key: 'treatment.name',
+    }, {
+      title: 'หมอ',
+      dataIndex: 'slot.dentist',
+      key: 'slot.dentist',
+      render: (dent) => <p>{`${dent.firstname} ${dent.lastname}`}</p>
+    }
+  ]  
+
+  return { dataSource: appointments, columns }
 }
 
+
+// const AppItem = ({ appointment }) => {
+//   const { slot, status, report } = appointment
+//   console.log(appointment)
+//   const { dataSource, columns } = formatAppointments(appointments)
+//   return (
+//     <AppItemContainer>
+//       <Table dataSource={dataSource} columns={columns} />
+//     </AppItemContainer>
+//   )
+// }
+
 const Index = ({ appointments }) => {
+  const { dataSource, columns } = formatAppointments(appointments)
+
   return (
     <Container>
       appointment
       <AppContainer>
-        { _.map(appointments, (app) => <AppItem appointment={app} />)}
+        <Table dataSource={dataSource} columns={columns} />
+        {/* { _.map(appointments, (app) => <AppItem appointment={app} />)} */}
       </AppContainer>
     </Container>
   )
